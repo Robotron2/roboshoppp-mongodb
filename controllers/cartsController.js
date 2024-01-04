58a74d2c6f54a8786f45fbe4df801e90f78b8e1d
Theophilus Adeleke <theophilusadesola002@gmail.com>
2024-01-04 23:02:03 +0100

    Progress: User can now add different products to cart and if there is an existing product, it sets the quantity to the new quantity specified.

--- controllers/cartsController.js
+++ controllers/cartsController.js
@@ -1,339 +1,6 @@
 import { User } from "../models/usersModel.js"
-import { Cart } from "../models/cartsModel.js"
 import { CartItem } from "../models/cartItemModel.js"
 
-// export const addToCartController = async (req, res) => {
-// 	const { userId } = req.user
-// 	const { productId, quantity } = req.body
-
-// 	try {
-// 		let user = await User.findById(userId).populate("cart")
-// 		// let user = await User.findById(userId)
-
-// 		if (!user.cart) {
-// 			const cart = new Cart({ user: userId, cartItems: [] })
-// 			user.cart = cart._id
-// 			await cart.save()
-// 		}
-// 		console.log(user)
-
-// 		const existingCart = await Cart.findOne({ user: userId })
-
-// 		if (existingCart) {
-// 			// Update existing cart
-// 			existingCart.cartItems.push(newCartItem)
-// 			await existingCart.save()
-// 		} else {
-// 			// Create a new cart
-// 			const cart = new Cart({ user: userId, cartItems: [newCartItem] })
-// 			await cart.save()
-// 			user.cart = cart._id
-// 			await user.save()
-// 		}
-
-// 		await user.cart.save()
-// 		await user.save()
-
-// 		res.status(200).json({ success: true, message: "Product added to cart successfully" })
-// 	} catch (error) {
-// 		console.error(error)
-// 		res.status(500).json({ error: "Internal Server Error", success: false })
-// 	}
-// }
-
-// export const addToCartController = async (req, res) => {
-// 	const { userId } = req.user
-// 	const { productId, quantity } = req.body
-
-// 	try {
-// 		// Find user and populate cart
-// 		let user = await User.findOne({ _id: userId }).populate("cart")
-
-// 		// If user does not have a cart, create a new one
-// 		if (!user.cart) {
-//             // Create a new cart
-// 			const cart = new Cart({ user: userId, cartItems: [] })
-// 			user.cart = cart._id
-// 			await cart.save()
-// 			await user.save()
-// 		}
-
-// 		// Check for existing cart
-// 		const existingCart = await Cart.findOne({ user: userId })
-
-// 		// Create a new cart item
-// 		const newCartItem = new CartItem({
-// 			quantity,
-// 			product: productId,
-// 		})
-
-// 		if (existingCart) {
-// 			existingCart.cartItems.push(newCartItem)
-// 			await existingCart.save()
-// 		} else {
-// 			// Create a new cart
-// 			const cart = new Cart({ user: userId, cartItems: [newCartItem] })
-// 			await cart.save()
-// 			user.cart = cart._id
-// 			await user.save()
-// 		}
-
-// 		res.status(200).json({ success: true, message: "Product added to cart successfully" })
-// 	} catch (error) {
-// 		console.error(error)
-// 		res.status(500).json({ error: "Internal Server Error", success: false })
-// 	}
-// }
-
-// export const addToCartController = async (req, res) => {
-// 	const { userId } = req.user
-// 	const { productId, quantity } = req.body
-
-// 	try {
-// 		// Find user and populate cart
-// 		let user = await User.findOne({ _id: userId }).populate("cart")
-
-// 		// If user does not have a cart, create a new one
-// 		if (!user.cart) {
-// 			// Create a new cart
-// 			const cart = new Cart({ user: userId, cartItems: [] })
-// 			user.cart = cart._id
-// 			await cart.save()
-// 			await user.save()
-// 		}
-
-// 		// Check for existing cart item for the product
-// 		const existingCartItem = user.cart.cartItems.find((item) => item.product.toString() === productId)
-
-// 		if (existingCartItem) {
-// 			// If the product already exists in the cart, update the quantity
-// 			existingCartItem.quantity += quantity
-// 			await user.cart.save()
-// 		} else {
-// 			// Create a new cart item
-// 			const newCartItem = new CartItem({
-// 				quantity,
-// 				product: productId,
-// 			})
-
-// 			// Add the new cart item to the cart
-// 			user.cart.cartItems.push(newCartItem)
-// 			await user.cart.save()
-// 		}
-
-// 		res.status(200).json({ success: true, message: "Product added to cart successfully" })
-// 	} catch (error) {
-// 		console.error(error)
-// 		res.status(500).json({ error: "Internal Server Error", success: false })
-// 	}
-// }
-
-// export const addToCartController = async (req, res) => {
-// 	const { userId } = req.user
-// 	const { productId, quantity } = req.body
-
-// 	try {
-// 		// Find user and populate cart
-// 		let user = await User.findOne({ _id: userId }).populate("cart")
-
-// 		if (!user.cart) {
-// 			// Create a new cart
-// 			const cart = new Cart({ user: userId, cartItems: [] })
-// 			user.cart = cart._id
-// 			await cart.save()
-// 			await user.save()
-// 		}
-
-// 		const existingCartItem = user.cart.cartItems.find((item) => item.product && item.product.toString() === productId)
-
-// 		if (existingCartItem) {
-// 			// If the product already exists in the cart, update the quantity
-// 			console.log(existingCartItem.quantity)
-// 			existingCartItem.quantity += quantity
-// 			await user.cart.save()
-// 		} else {
-// 			// Create a new cart item
-// 			const newCartItem = new CartItem({
-// 				quantity,
-// 				product: productId,
-// 			})
-// 			user.cart.cartItems.push(newCartItem)
-// 			await newCartItem.save()
-// 			await user.cart.save()
-// 		}
-
-// 		const updatedUser = await User.findOne({ _id: userId }).populate({
-// 			path: "cart",
-// 			populate: {
-// 				path: "cartItems",
-// 			},
-// 		})
-
-// 		res.status(200).json({ success: true, message: "Product added to cart successfully", cart: updatedUser.cart })
-// 	} catch (error) {
-// 		console.error(error)
-// 		res.status(500).json({ error: "Internal Server Error", success: false })
-// 	}
-// }
-
-// export const addToCartController = async (req, res) => {
-// 	const { userId } = req.user
-// 	const { productId, quantity } = req.body
-
-// 	try {
-// 		// Find user and populate cart
-// 		const user = await User.findById(userId).populate("cart")
-
-// 		// If user does not have a cart, create a new one
-// 		if (!user.cart) {
-// 			const cart = new Cart({ user: userId, cartItems: [] })
-// 			user.cart = cart._id
-// 			await cart.save()
-// 			await user.save()
-// 		}
-
-// 		// Check for existing cart item for the product
-// 		const existingCartItem = user.cart.cartItems.find((item) => item.product.toString() === productId)
-
-// 		if (existingCartItem) {
-// 			// If the product already exists in the cart, update the quantity
-// 			existingCartItem.quantity += quantity
-// 			await existingCartItem.save()
-// 		} else {
-// 			// Create a new cart item
-// 			const newCartItem = new CartItem({
-// 				quantity,
-// 				product: productId,
-// 			})
-// 			user.cart.cartItems.push(newCartItem)
-// 			await user.cart.save()
-// 		}
-
-// 		const updatedUser = await User.findById(userId).populate({
-// 			path: "cart",
-// 			populate: {
-// 				path: "cartItems",
-// 			},
-// 		})
-
-// 		res.status(200).json({
-// 			success: true,
-// 			message: "Product added to cart successfully",
-// 			cart: updatedUser.cart,
-// 		})
-// 	} catch (error) {
-// 		console.error(error)
-// 		res.status(500).json({ error: "Internal Server Error", success: false })
-// 	}
-// }
-
-// export const addToCartController = async (req, res) => {
-// 	const { userId } = req.user
-// 	const { productId, quantity } = req.body
-
-// 	try {
-// 		// Find user and populate cart
-// 		const user = await User.findById(userId).populate("cart")
-
-// 		// If user does not have a cart, create a new one
-// 		if (!user.cart) {
-// 			const cart = new Cart({ user: userId, cartItems: [] })
-// 			user.cart = cart._id
-// 			await cart.save()
-// 			await user.save()
-// 		}
-
-// 		// Check for existing cart item for the product
-// 		if (!user.cart.cartItems) {
-// 			user.cart.cartItems = []
-// 		}
-
-// 		const existingCartItem = user.cart.cartItems.find((item) => item.product && item.product.toString() === productId)
-
-// 		if (existingCartItem) {
-// 			// If the product already exists in the cart, update the quantity
-// 			existingCartItem.quantity += quantity
-// 			await existingCartItem.save()
-// 		} else {
-// 			// Create a new cart item
-// 			const newCartItem = new CartItem({
-// 				quantity,
-// 				product: productId,
-// 			})
-// 			user.cart.cartItems.push(newCartItem)
-// 			await user.cart.save()
-// 		}
-
-// 		const updatedUser = await User.findById(userId).populate({
-// 			path: "cart",
-// 			populate: {
-// 				path: "cartItems",
-// 			},
-// 		})
-
-// 		res.status(200).json({
-// 			success: true,
-// 			message: "Product added to cart successfully",
-// 			cart: updatedUser.cart,
-// 		})
-// 	} catch (error) {
-// 		console.error(error)
-// 		res.status(500).json({ error: "Internal Server Error", success: false })
-// 	}
-// }
-
-// export const addToCartController = async (req, res) => {
-// 	const { userId } = req.user
-// 	const { productId, quantity } = req.body
-
-// 	try {
-// 		// Find user and populate cart
-// 		let user = await User.findById(userId).populate("cart")
-
-// 		// If user does not have a cart, create a new one
-// 		if (!user.cart) {
-// 			const cart = new Cart({ user: userId, cartItems: [] })
-// 			user.cart = cart._id
-// 			await cart.save()
-// 			await user.save()
-// 		}
-
-// 		// Check for existing cart item for the product
-// 		let existingCartItem = user.cart.cartItems.find((item) => item.product && item.product.toString() === productId)
-
-// 		if (existingCartItem) {
-// 			// If the product already exists in the cart, update the quantity
-// 			existingCartItem.quantity += quantity
-// 			await existingCartItem.save()
-// 		} else {
-// 			// Create a new cart item
-// 			const newCartItem = new CartItem({
-// 				quantity,
-// 				product: productId,
-// 			})
-// 			await newCartItem.save()
-// 			user.cart.cartItems.push(newCartItem)
-// 			await user.cart.save()
-// 		}
-
-// 		// Populate cartItems before sending the response
-// 		user = await User.findById(userId).populate({
-// 			path: "cart",
-// 			populate: {
-// 				path: "cartItems",
-// 			},
-// 		})
-
-// 		res.status(200).json({
-// 			success: true,
-// 			message: "Product added to cart successfully",
-// 			cart: user.cart,
-// 		})
-// 	} catch (error) {
-// 		console.error(error)
-// 		res.status(500).json({ error: "Internal Server Error", success: false })
-// 	}
-// }
 export const addToCartController = async (req, res) => {
 	const { userId } = req.user
 	const { productId, quantity } = req.body
@@ -343,7 +10,7 @@ export const addToCartController = async (req, res) => {
 		if (!user) {
 			throw Error("Please login and try again.")
 		}
-		const userCart = user.cart
+		let userCart = user.cart
 
 		const newCartItem = new CartItem({
 			quantity,
@@ -357,21 +24,43 @@ export const addToCartController = async (req, res) => {
 		const existingCartItem = userCart.filter((item) => {
 			return item.product.toString() === productId
 		})
-		if (!existingCartItem) {
+
+		const nonExistingCartItem = userCart.filter((item) => {
+			return item.product.toString() !== productId
+		})
+
+		if (existingCartItem.length === 0) {
 			userCart.push(newCartItem)
+			user.cart = userCart
+			await user.save()
+		} else {
+			const updatedItemQuantity = (existingCartItem[0].quantity = quantity)
+			const updatedCartItem = new CartItem({ quantity: updatedItemQuantity, product: productId })
+			userCart = [...nonExistingCartItem, updatedCartItem]
 			user.cart = userCart
 			await user.save()
 		}
 
-		const updatedItemQuantity = (existingCartItem[0].quantity = quantity)
-		userCart.push(updatedItemQuantity)
-		user.cart = userCart
-		await user.save()
+		// const userCartContents = await User.findById(userId).populate({ path: "cart", populate: "product" })
+		// const userCartContents = await User.findById(userId)
+		// 	.populate({
+		// 		path: "cart",
+		// 		populate: {
+		// 			path: "cartItems",
+		// 			populate: {
+		// 				path: "product",
+		// 				select: "name description price",
+		// 			},
+		// 		},
+		// 	})
+		// 	.select("id")
 
 		return res.status(200).json({
 			success: true,
 			message: "Product added to cart successfully",
 			cart: user.cart,
+			cartLength: user.cart.length,
+			userCartContents,
 		})
 	} catch (error) {
 		console.error(error)
